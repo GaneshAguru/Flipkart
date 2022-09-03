65f243b3f74f5361178319a0df30f5496b86c756
Ganesh Aguru <ganeshaguru07@gmail.com>
2022-09-03 14:01:33 +0530

    dark and night mode

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const toggle1=document.getElementById('toggleDark');
+const body=document.querySelector('body');
+
+toggle1.addEventListener('click',function(){
+    this.classList.toggle('bi-moon-stars');
+    if(this.classList.toggle("bi-brightness-high-fill")){
+        body.style.background='white';
+        body.style.color='black';
+        body.style.transition='1.5s'
+    }
+    else{
+        body.style.background='black';
+        body.style.color='white';
+        body.style.transition='1.5s'
+
+    }
+})
