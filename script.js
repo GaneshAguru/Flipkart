07fccd9db4a1aedef49b11bfe99ef0cea7f59385
Ganesh Aguru <ganeshaguru07@gmail.com>
2022-09-12 22:06:40 +0530

    updated

--- script.js
+++ script.js
@@ -21,15 +21,7 @@ toggle1.addEventListener('click',function(){
 
 // ////// coupon code
 
-function loadCoupon(){
-    document.getElementById('coupon').style.display="block";
-    document.getElementById('search').style.opacity="0.5";
-}
 
-const closeCoupon = () => {
-    document.getElementById('coupon').style.display="none";
-    document.getElementById('search').style.opacity="1";
-}
 
 
 // //////
